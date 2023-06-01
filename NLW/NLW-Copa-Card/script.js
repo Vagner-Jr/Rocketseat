b69dfd4d55426619b564f5a4c834dc8ee380df44
Vagner Jr <vagnerflorindojunior@gmail.com>
2023-05-31 21:41:30 -0300

    deleta repositório nlw
    deleta repositório nlw

--- NLW/NLW-Copa-Card/script.js
+++
@@ -1,6 +0,0 @@
-const card = document.querySelector('.card')
-
-card.addEventListener('click', () => {
-    const html = document.documentElement; // pega o elememto root
-    html.classList.toggle('ignite')
-})
